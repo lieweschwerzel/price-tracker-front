4c9bec0ad47462e1785cffec3d9acbd3c34d6aa0
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-11-19 17:35:09 +0100

    build prod

---
+++ build/precache-manifest.0f460ea06ba474e70390e6cd928a49d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a6cbc5052f03e6dc74994d8166ed5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e090708531c3a9d645",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "b09571eaae9585977c27",
+    "url": "/static/css/main.0672905c.chunk.css"
+  },
+  {
+    "revision": "53e090708531c3a9d645",
+    "url": "/static/js/2.9719836f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.9719836f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55d5ad0700cdf5296ba",
+    "url": "/static/js/3.67363bdb.chunk.js"
+  },
+  {
+    "revision": "b09571eaae9585977c27",
+    "url": "/static/js/main.794561fe.chunk.js"
+  },
+  {
+    "revision": "60a5ba07d11bb35e8ae5",
+    "url": "/static/js/runtime-main.af3ef0e2.js"
+  }
+]);
