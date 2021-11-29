1aacb7af7eb3508f2ee836ee84e58a7cd8aea883
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-11-29 17:13:21 +0100

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,17 +6,16 @@ function TrackedItem(props) {
         axios.delete(`https://price-tracker-back.herokuapp.com/api/item/${title}`)
             .then(res => {
                 console.log("Clicked from Grandchild");
-                console.log(props.item.img_url)
                 props.test();
             })
     }
 
-    if (!props.item.mail ) {
+    if (!props.item.mail) {
         return (
             <div>
                 <p>
                     <span>
-                    </span>                            
+                    </span>
                 </p>
             </div>
         )
@@ -24,13 +23,12 @@ function TrackedItem(props) {
     else {
         return (
             <div>
-                <p> 
-                   
-                    <span style={{ fontWeight: 'bold, underline' , flex: 1, flexDirection: 'row'}}>
-                        <img src={props.item.img_url} alt="ahimage" style={{width: '100px', height: '50%'}}/>
-                       
-                    {props.item.title}: €</span>{props.item.price}
-                    <button onClick={() => deleteitemHandler(props.item.title)} className="btn btn-outline-danger my-2 mx-2" style={{ 'borderRadius': '30px', flex:'1' }}>X</button>
+                <p>
+                    <button onClick={() => deleteitemHandler(props.item.title)} className="btn btn-outline-danger my-2 mx-2" style={{ 'borderRadius': '30px', flex: '1' }}>X</button>
+                    <span style={{ fontWeight: 'bold, underline', flex: 1, flexDirection: 'row' }}>
+                        <img src={props.item.img_url} alt="ahimage" style={{ width: '100px', height: '50%' }} />
+
+                        {props.item.title}: €</span>{props.item.price}
                     <hr></hr>
                 </p>
             </div>
